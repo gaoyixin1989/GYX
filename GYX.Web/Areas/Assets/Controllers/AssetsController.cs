0c964da1e2835c34c7590b09e8f0d5b9a8e12f69
gyx <654747667@qq.com>
2017-10-09 18:07:18 +0800

    11

--- GYX.Web/Areas/Assets/Controllers/AssetsController.cs
+++ GYX.Web/Areas/Assets/Controllers/AssetsController.cs
@@ -39,6 +39,18 @@ namespace GYX.Web.Areas.Assets.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 统计明细编辑页面
+        /// </summary>
+        /// <param name="AssetsId">资产统计id</param>
+        /// <param name="DetailId">明细id</param>
+        /// <returns></returns>
+        public ActionResult EditDetail(Guid? AssetsId, Guid? DetailId)
+        {
+            ViewData["AssetsId"] = AssetsId;
+            ViewData["DetailId"] = DetailId;
+            return View();
+        }
 
         #region 统计情况
         #region 数据查询
@@ -197,6 +209,25 @@ namespace GYX.Web.Areas.Assets.Controllers
         #endregion
 
         #region 统计明细
+        
+
+        #region 数据查询
+        /// <summary>
+        /// 根据id查询明细
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public JsonResult GetDataById_Detail(Guid? id)
+        {
+            var obj = _detailService.FindById(id);
+            return BackData(obj);
+        }
+
+        /// <summary>
+        /// 根据统计id查询对应明细列表
+        /// </summary>
+        /// <param name="AssetsId"></param>
+        /// <returns></returns>
         public JsonResult GetDataList_Detail(Guid? AssetsId)
         {
             var listData = _assetsService.FindById(AssetsId)?.DetailList;
@@ -206,9 +237,111 @@ namespace GYX.Web.Areas.Assets.Controllers
                 rows = listData
             });
         }
-        #region 数据查询
+
         #endregion
         #region 数据编辑
+        /// <summary>
+        /// 保存数据
+        /// </summary>
+        public JsonResult Save_Detail(AssetsDetail model)
+        {
+            if (model.Id == Guid.Empty)
+                return Create_Detail(model);
+            else
+                return Update_Detail(model);
+        }
+
+        /// <summary>
+        /// 添加数据
+        /// </summary>
+        private JsonResult Create_Detail(AssetsDetail model)
+        {
+            SystemResult result = new SystemResult();
+            model.Id = Guid.NewGuid();
+
+            try
+            {
+                if (_detailService.Insert(model))
+                    result.isSuccess = true;
+                else
+                {
+                    result.isSuccess = false;
+                    result.message = "新增失败";
+                }
+            }
+            catch (Exception ex)
+            {
+                result.isSuccess = false;
+                result.message = ex.Message;
+            }
+            return BackData(result);
+        }
+        /// <summary>
+        /// 修改数据
+        /// </summary>
+        private JsonResult Update_Detail(AssetsDetail model)
+        {
+            SystemResult result = new SystemResult();
+
+            if (_detailService.Update(model))
+                result.isSuccess = true;
+            else
+            {
+                result.isSuccess = false;
+                result.message = "更新失败";
+            }
+
+            return BackData(result);
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        public JsonResult Delete_Detail(Guid[] ids)
+        {
+            SystemResult result = new SystemResult();
+            int intSuccess = 0;
+            int intError = 0;
+            //Delete
+            try
+            {
+                var objs = _detailService.List().Where(u => ids.Contains(u.Id)).ToList();
+                if (objs.Count > 0)
+                {
+                    foreach (var obj in objs)
+                    {
+                        if (_detailService.Delete(obj))
+                            intSuccess++;
+                        else
+                            intError++;
+                    }
+                    if (intSuccess > 0)
+                    {
+                        result.isSuccess = true;
+                        result.message = string.Format("成功删除{0}条数据，失败{1}条", intSuccess, intError);
+                    }
+                    else
+                    {
+                        result.isSuccess = false;
+                        result.message = "删除数据失败";
+                    }
+
+                }
+                else
+                {
+                    result.isSuccess = false;
+                    result.message = "没有可删除的数据";
+                }
+            }
+            catch (Exception ex)
+            {
+                result.isSuccess = false;
+                result.message = ex.Message;
+            }
+            return BackData(result);
+        }
         #endregion
         #endregion
     }
